b782b7c3079f13f458be57630ffbba693ba551ef
truongdat2411 <tdat4926@gmail.com>
2025-03-06 16:35:35 +0700

    release version 4

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -83,9 +83,10 @@ def update(id):
                 (title, body, id)
             )
             db.commit()
-            return redirect(url_for('blog.index'))
+            return redirect(url_for('blog.my_posts'))
 
     return render_template('blog/update.html', post=post)
+
 
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
@@ -94,4 +95,55 @@ def delete(id):
     db = get_db()
     db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
-    return redirect(url_for('blog.index'))
+    return redirect(url_for('blog.my_posts'))
+
+
+@bp.route('/delete_multiple', methods = ['POST'])
+@login_required
+def delete_multiple():
+    post_ids = request.form.getlist('post_ids')  
+    if not post_ids:
+        flash('No posts selected for deletion.')
+        return redirect(url_for('blog.my_posts'))
+    
+    db = get_db()
+    for post_id in post_ids:
+        try:
+            post = get_post(post_id, check_author=True)
+            db.execute('DELETE FROM post WHERE id = ?', (post_id,))
+        except abort:
+            flash(f'Error deleting post ID {post_id}. You may not have permission.')
+            continue
+    
+    db.commit()
+    flash(f'Successfully deleted {len(post_ids)} posts.')
+    return redirect(url_for('blog.my_posts'))
+
+@bp.route('/my-posts')
+@login_required
+def my_posts():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.author_id = ?'
+        ' ORDER BY created DESC',
+        (g.user['id'],)
+    ).fetchall()
+    return render_template('blog/my_posts.html', posts=posts)
+
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
